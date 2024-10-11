45f8e5cb9d37bced9481819cd6e7fce19fdd8c8b
GilsonAndre <gilsonandre1999@gmail.com>
2024-10-10 21:46:06 -0300

    created strings page

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:form_validade/resources/strings.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(Strings.register),
+      ),
+    );
   }
 }
