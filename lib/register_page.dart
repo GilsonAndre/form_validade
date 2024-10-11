52028637b7a81245efc78c4356f1421bc354011e
GilsonAndre <gilsonandre1999@gmail.com>
2024-10-10 22:03:09 -0300

    base for textformfield

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -10,6 +10,33 @@ class RegisterPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(Strings.register),
       ),
+      body: SingleChildScrollView(
+        child: Form(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                TextFormField(
+                  autocorrect: true,
+                  decoration: InputDecoration(
+                    hintText: Strings.hintName,
+                    label: const Text(Strings.hintName),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -1,3 +1,11 @@
 class Strings {
   static const register = 'Register';
+
+  //HINT TEXT
+  static const hintName = 'Name';
+  // static const hint = '';
+  // static const hint = '';
+  // static const hint = '';
+  // static const hint = '';
+
 }
