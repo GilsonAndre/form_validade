511b4cd8378bb3eea6a8ad3ddeaaff012e62e04e
GilsonAndre <gilsonandre1999@gmail.com>
2024-10-16 16:28:40 -0300

    mask and regex added

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:form_validade/resources/strings.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class RegisterPage extends StatelessWidget {
   RegisterPage({super.key});
   final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    MaskTextInputFormatter maskBrazilianCpf = MaskTextInputFormatter(mask: '###.###.###-##');
+    MaskTextInputFormatter maskDateTime = MaskTextInputFormatter(mask: '##/##/####');
+    MaskTextInputFormatter maskBrazilianPhone = MaskTextInputFormatter(mask: '(##)#####-####');
+
+    RegExp regexEmail = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    RegExp regexPassword = RegExp(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$');
+    RegExp regexDateTime = RegExp(r'^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[12][0-9]{3}$');
     return Scaffold(
       appBar: AppBar(
         title: const Text(Strings.register),
@@ -24,9 +32,13 @@ class RegisterPage extends StatelessWidget {
                     hintText: Strings.hintName,
                     label: Text(Strings.hintName),
                   ),
+                  inputFormatters: [maskDateTime],
                   validator: (value) {
                     if (value!.isEmpty) {
                       return 'Nome não pode ser vazio';
+                    }
+                    if (!regexDateTime.hasMatch(value)) {
+                      return 'Invalid date';
                     }
                     return null;
                   },
